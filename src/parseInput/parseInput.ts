63cca7b74bb7291f818b073e3c87af03f67c3c17
Alexis Pastouret <pastouret.alexis@gmail.com>
2020-10-11 23:17:44 +0200

    Create a parseInput method to parse a .txt file to have all mandatory information to run the algorithm

---
+++ src/parseInput/parseInput.ts
@@ -0,0 +1,63 @@
+const lineByLine = require("n-readlines");
+
+export function parseInput(inputPath) {
+    const readLineInterface = buildReadlineInterface(inputPath);
+    let line = readLineInterface.next();
+    const { lawnColumns, lawnRows } = parseLawnDimensions(line);
+    const mowersPositionAndInstructions = parseMowersPositionAndInstructions(
+        readLineInterface
+    );
+
+    return {
+        lawnColumns,
+        lawnRows,
+        mowersPositionAndInstructions,
+    };
+}
+
+function buildReadlineInterface(filePath) {
+    return new lineByLine(filePath);
+}
+
+function parseMowersPositionAndInstructions(readLineInterface) {
+    const result = [];
+    let line;
+    while ((line = readLineInterface.next())) {
+        const {
+            initialColumn,
+            initialOrientation,
+            initialRow,
+        } = parseInitiationPosition(line);
+        line = readLineInterface.next();
+        const instructions = parseLawnInstructions(line);
+        result.push({
+            initialColumn,
+            initialOrientation,
+            initialRow,
+            instructions,
+        });
+    }
+    return result;
+
+    function parseInitiationPosition(line) {
+        const [
+            initialRow,
+            initialColumn,
+            initialOrientation,
+        ] = line.toString().split(" ");
+        return {
+            initialColumn: parseInt(initialColumn),
+            initialOrientation,
+            initialRow: parseInt(initialRow),
+        };
+    }
+
+    function parseLawnInstructions(line) {
+        return line.toString().split("");
+    }
+}
+
+function parseLawnDimensions(line) {
+    const [lawnRows, lawnColumns] = line.toString().split(" ").map(Number);
+    return { lawnColumns, lawnRows };
+}
---
+++ src/parseInput/parseInput.unit.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import { parseInput } from "./parseInput";
+
+describe("parseInput", () => {
+    it("should return the lawn columns, rows and mowers initial position and instructions", () => {
+        const result = parseInput(`${__dirname}/testInput.txt`);
+
+        expect(result).to.deep.equal({
+            lawnColumns: 5,
+            lawnRows: 5,
+            mowersPositionAndInstructions: [
+                {
+                    initialColumn: 2,
+                    initialOrientation: "N",
+                    initialRow: 1,
+                    instructions: ["L", "F", "L", "F", "L", "F", "L", "F", "F"],
+                },
+                {
+                    initialColumn: 3,
+                    initialOrientation: "E",
+                    initialRow: 3,
+                    instructions: [
+                        "F",
+                        "F",
+                        "R",
+                        "F",
+                        "F",
+                        "R",
+                        "F",
+                        "R",
+                        "R",
+                        "F",
+                    ],
+                },
+            ],
+        });
+    });
+});
