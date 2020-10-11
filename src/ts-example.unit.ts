493d113393c3e40e301c2434bfa4a7ce50ac600d
Alexis Pastouret <pastouret.alexis@gmail.com>
2020-10-11 11:08:03 +0200

    Remove useless ts-example and its unit test

--- src/ts-example.unit.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { helloWorld } from "./ts-example";
-
-describe("ts-example", () => {
-    it("should say 'Hello World!'", () => {
-        expect(helloWorld()).to.equal("Hello World!");
-    });
-});
